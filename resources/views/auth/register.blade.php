4d5ef4aafdecc76862fb4f89c827143223449ff0
Victor Juan <vj.jimenez96@gmail.com>
2019-01-22 23:37:00 -0500

    shop

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.app')
+@section('body-class','login-page')
+@section('content')
+<div class="page-header header-filter" style="background-image: url('{{asset('img/bg7.jpg')}}'); background-size: cover; background-position: top center;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+          <div class="card card-login">
+            <form class="form" method="POST" action="{{ route('register') }}">
+                @csrf
+                <div class="card-header card-header-primary text-center">
+                    <h4 class="card-title">Registro</h4>
+                    {{-- <div class="social-line">
+                      <a href="#pablo" class="btn btn-just-icon btn-link">
+                        <i class="fa fa-facebook-square"></i>
+                    </a>
+                    <a href="#pablo" class="btn btn-just-icon btn-link">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a href="#pablo" class="btn btn-just-icon btn-link">
+                        <i class="fa fa-google-plus"></i>
+                    </a>
+                </div> --}}
+            </div>
+            <p class="description text-center">Completa tus datos</p>
+            <div class="card-body">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">face</i>
+                  </span>
+              </div>
+              <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" placeholder="Nombres" required autofocus>
+              @if ($errors->has('name'))
+              <span class="invalid-feedback" role="alert">
+                <strong>{{ $errors->first('name') }}</strong>
+            </span>
+            @endif
+        </div>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="material-icons">mail</i>
+          </span>
+      </div>
+      <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }} " placeholder="Email" required autofocus>
+      @if ($errors->has('email'))
+      <span class="invalid-feedback" role="alert">
+        <strong>{{ $errors->first('email') }}</strong>
+    </span>
+    @endif
+</div>
+<div class="input-group">
+  <div class="input-group-prepend">
+    <span class="input-group-text">
+      <i class="material-icons">lock_outline</i>
+  </span>
+</div>
+<input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Contrasenia" required>
+</div>
+<div class="input-group">
+  <div class="input-group-prepend">
+    <span class="input-group-text">
+      <i class="material-icons">lock_outline</i>
+  </span>
+</div>
+<input id="password-confirm" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password_confirmation" placeholder="Confirmar Contrasenia" required>
+</div>
+</div>
+<div class="input-group">
+  <div class="checkbox">
+      <label>
+        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+        Recordar Sesión
+    </label>
+</div>
+</div>
+{{-- <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a> --}}
+                                <div class="footer text-center">
+                                    <button type="submit" class="btn btn-primary btn-link btn-wd btn-lg">Empezar</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <footer class="footer">
+                  <div class="container">
+                    <nav class="float-left">
+                      <ul>
+                        <li>
+                          <a href="https://www.creative-tim.com">
+                            Creative Tim
+                        </a>
+                    </li>
+                    <li>
+                      <a href="https://creative-tim.com/presentation">
+                        About Us
+                    </a>
+                </li>
+                <li>
+                  <a href="http://blog.creative-tim.com">
+                    Blog
+                </a>
+            </li>
+            <li>
+              <a href="https://www.creative-tim.com/license">
+                Licenses
+            </a>
+        </li>
+    </ul>
+</nav>
+<div class="copyright float-right">
+  &copy;
+  <script>
+    document.write(new Date().getFullYear())
+</script>, made with <i class="material-icons">favorite</i> by
+<a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> for a better web.
+</div>
+</div>
+</footer>
+</div>
+@endsection
